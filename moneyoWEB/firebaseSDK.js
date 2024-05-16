3ff837f01baf9f4feb538465acb5ddef6e4f498f
pandajakob <jakobm.dev@gmail.com>
2024-05-16 09:33:06 +0800

    firebase integrated to webApp

--- moneyoWEB/firebaseSDK.js
+++ moneyoWEB/firebaseSDK.js
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
-import { 
-    getFirestore, collection, getDocs,
-    addDoc
-} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyBxUijoVOg787I6GrOcSZIpSwGdNE6_Tqk",
     authDomain: "moneyo-4567d.firebaseapp.com",
     projectId: "moneyo-4567d",
@@ -20,41 +18,64 @@ const firebaseConfig = {
     measurementId: "G-R7RYSS1Q77"
   };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 
-const db = getFirestore()
-const bubblesColRef = collection(db, "bubbles")
 
-let mainElement = document.getElementById("main")
+  const db = getFirestore();
+  const bubblesColRef = collection(db, "bubbles")
+  
+  const mainElement = document.getElementById("main")
+  
+  getDocs(bubblesColRef)
+  .then((snapshot) => {
+      let bubbles = [];
+      snapshot.docs.forEach((doc) => {
+          bubbles.push({ ...doc.data(), id: doc.id });
+          console.log({...doc.data(), id: doc.id});
+      })
+      
+      /*
+      const sortedData = bubbles.sort((a, b) => {
+          const dateA = a.timestamp.toDate();
+          const dateB = b.timestamp.toDate();
+          return dateA - dateB;
+      });
+      */
+      
+      bubbles.forEach((bubble) => {
+        console.log(bubble.name)
+        let parent = document.getElementById("bubbles")
 
-getDocs(bubblesColRef)
-.then((snapshot) => {
-    let bubbles = [];
 
-    snapshot.docs.forEach((doc) => {
-        bubbles.push({ ...doc.data(), id: doc.id });
-    })
+        let bubbleButton = document.createElement("button");
+        bubbleButton.className = "bubbleButton"
+        bubbleButton.style.backgroundColor = colorOfBubble(bubble.color);
 
-    const sortedData = bubbles.sort((a, b) => {
-        const dateA = a.timestamp.toDate();
-        const dateB = b.timestamp.toDate();
-        return dateA - dateB;
-    });
-    bubbles.forEach((bubble) => {
-        let bubbleDiv = document.getElementByTagName("main").createElement("div");
-        bubbleDiv.className = "bubble"
-
+  
         const bubbleName = document.createElement("p")
-        text.innerHTML =   bubble.name;
-        text.className = "text";
+        bubbleName.innerHTML =   bubble.name;
+        bubbleName.className = "bubbleName";
 
-        bubbleDiv.appendChild(bubbleName);
-        mainElement.appendChild(bubbleDiv)
-    })
-})
-.catch(err => {
-    console.log(err);
-})
+        bubbleButton.appendChild(bubbleName);
+        parent.appendChild(bubbleButton);
+      })
+  })
+  .catch(err => {
+      console.log(err);
+  })
+  
+  function colorOfBubble(colorName) {
+    switch(colorName) {
+       case "blue": return "#2986cc" 
+       case "red": return "FF0000"
+       case "yellow": return "#ffd966"
+       case "orange": return "#e69138"
+       case "green": return "#6aa84f"
+       case "cyan": return "#00FFFF"
+    }
+  
+
+  }
